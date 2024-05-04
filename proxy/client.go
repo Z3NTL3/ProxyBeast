d42db519c6de0d3f07349f81eefee1b4eb70ba07
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2024-05-04 15:28:13 +0200

    .

--- proxy/client.go
+++ proxy/client.go
@@ -1,63 +1,16 @@
 package proxy
 
-import "os"
+import "context"
 
 type (
 	/*
-		not final
+		TODO
 	*/
 	Anonimity string
 	Proxy     string
-
-	/*
-		This client is lightweight and very unique as it is working with
-		several pools, in an event driven manner.
-
-
-		Take the following as example:
-
-		Before starting any worker, the fd_pool workers gets spawned.
-		They can process 200 file writes at the same time, while only
-		one file descriptor stays open, until the end.
-
-		The proxies in the provided proxy list get's read line by line
-		in the main thread. Everytime a line is read it will start a goroutine, which
-		just checks one given proxy. Plus incrementing ``current``.
-
-		This means all your proxies are checked in isolated environments and all in parallel
-		with each other. Once one finalizes it i'll decrement one of ``current``
-
-		After those workers get spawned, the pool watcher is waiting until
-		all goroutines have finished, if in the meantime the user interrupts the operation
-		for example with CTRL + C it will kill and stop all workers and finally exit.
-
-
-	*/
-	ProxyClient struct {
-		*os.File // one open file socket to the 'saves' file until the end of the operation
-		current uint64 // current amount of running goroutines; 0 means all tasks finished
-
-		/*
-			Transmits data to the channel
-			in order for the good proxies to be written into
-			the 'saves' file.
-
-			FD pool has concurrent goroutines that listen for
-			incoming transmits, in parallel
-		*/
-		fd_pool chan struct {
-			proxy     Proxy
-			latency   uint32
-			anonimity Anonimity
-		}
-
-		/*
-			Transmits proxies that need to be checked
-			to the worker channel. If given proxy is working
-			it will report its qualities and transmit further to the ``fd_pool``.
-		*/
-		workers chan struct {
-			proxy Proxy
-		}
-	}
 )
+
+func Run(ctx context.Context) {
+	// satisfies:
+	// the concept written down in README.md
+}
--- proxy/pool.go
+++ proxy/pool.go
@@ -1,6 +1,35 @@
 package proxy
 
-var Current uint32
+/*
+	About the concept, particularly how it operates with
+	several pools, in an event driven manner.
+
+
+	Take the following as example:
+
+	Before starting any worker, the fd_pool workers gets spawned.
+	They can process N amount of file writes at the same time, while only
+	one file descriptor stays open, until the end (just one no dup). Waiting
+	until they can consume from the channel which is buffered.
+
+	The proxies in the provided proxy list get's read line by line
+	in the main thread. Everytime a line is read it will push new worker into the worker pool, which
+	just checks one given proxy. Plus incrementing ``current``. The
+	worker pool is spawned after the FD_pool, they consume from the buffered
+	workers channel.
+
+	This means all your proxies are checked in isolated environments and all in parallel in a co-operative way
+	with each other. Once one finalizes it i'll decrement one of ``current``
+
+	After those workers get spawned, the pool watcher is waiting until
+	all goroutines have finished, if in the meantime the user interrupts the operation
+	for example with CTRL + C it will kill and stop all workers and finally exit.
+*/
+
+type Controller struct {
+	Current uint64 // current
+	Total uint64 // total work
+}
 
 type Workers chan struct {
 	proxy Proxy
