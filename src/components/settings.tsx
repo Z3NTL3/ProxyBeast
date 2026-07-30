d032b2e9f059e7781b99ed6a756cd7738a3a39c9
z3ntl3 <efdal.pix4@outlook.com>
2026-07-30 08:24:28 +0200

    07/30/2026 08:24:28

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -152,7 +152,7 @@ export default function Settings() {
   };
 
   return (
-    <div className="flex flex-col p-5 mx-8 mt-10">
+    <div className="flex flex-col p-5 mx-8 mt-3">
       <h1 className="text-2xl">Settings</h1>
       <p className="text-md text-gray-400">
         Configure your applications settings.
